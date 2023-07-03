d1d3196a7415022bea3e2d2c62ccb768ff359641
Vijay-bootlabs <vijay@bootlabstech.com>
2023-07-03 12:58:11 +0530

    initial commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type        = string
+  description = "name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of the resource group"
+}
+
+variable "nsg_name" {
+  type        = string
+  description = "name of the azurerm_network_security_group"
+}
+
+variable "nsg_rules" {
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  description = "values for each NSG rule"
+}
